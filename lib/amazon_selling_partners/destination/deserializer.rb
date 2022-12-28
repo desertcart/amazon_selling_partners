ec2a49703be991943ce6b6f052f973ccccfe6c38
Poula <bola.hanna55@gmail.com>
2022-12-28 10:06:00 +0100

    remove unnecessary require

--- lib/amazon_selling_partners/destination/deserializer.rb
+++ lib/amazon_selling_partners/destination/deserializer.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../pricing_offer/deserializer'
-
 module AmazonSellingPartners
   class Destination
     class Deserializer < AmazonSellingPartners::Deserializer
--- lib/amazon_selling_partners/subscription/operations/delete.rb
+++ lib/amazon_selling_partners/subscription/operations/delete.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'net/http'
-require 'uri'
-
 module AmazonSellingPartners
   class Subscription
     class Operation
